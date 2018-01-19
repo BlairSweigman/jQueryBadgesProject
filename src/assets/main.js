97986cb3fff2137fa07da623e42b26a02df2f2eb
BlairSweigman <blair.sweigman@gmail.com>
2018-01-19 16:27:21 -0500

    first try

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,22 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax({
+  	url: 'https://www.codeschool.com/users/BlairSweigman.json',
+  	
+  	dataType: 'jsonp',
+  	success: function(response) {
+  	$.each(response.courses.completed, function(index, course) {
+  			 /* iterate through array or object */
+  			 var strBadge = '<div class="course">';
+  			 strBadge +='<a href="' + course.url + '" target="_blank">';
+  			 strBadge +='<h3>' + course.title + '</h3>';
+  			 strBadge += '<img src="' + course.badge + '">';
+  			 strBadge += "</a>	</div>";
+  			 $('#badges').append(strBadge);
+  			 
+  		});
+  	}
+  });
+  
 });
